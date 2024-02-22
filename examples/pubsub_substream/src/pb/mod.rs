5174acc7f894f49070acc19b0e1d3a9259112c76
arnaudberger <arnaudberger84@gmail.com>
2024-02-22 18:38:21 -0500

    Adding .gitignore

---
+++ examples/pubsub_substream/src/pb/mod.rs
@@ -0,0 +1,8 @@
+// @generated
+pub mod pubsub {
+    // @@protoc_insertion_point(attribute:pubsub.v1)
+    pub mod v1 {
+        include!("pubsub.v1.rs");
+        // @@protoc_insertion_point(pubsub.v1)
+    }
+}
